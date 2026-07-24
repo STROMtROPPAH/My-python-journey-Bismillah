2c98a7c6eff748c7db02bc1ba87f2009e765b8b1
STROMtROPPAH <osurya254@gmail.com>
2026-07-24 22:22:56 +0800

    Add files via upload

---
+++ i love my cat.py
@@ -0,0 +1,11 @@
+cat_dct = {
+    'name' : 'motaz',
+    'color' : 'white mix orange',
+    'breed' : 'persian mix maicooon',
+    'age' : '4years',
+}
+keys = cat_dct.keys()
+value = cat_dct.values()
+
+print(len(cat_dct))
+print(keys + value)
