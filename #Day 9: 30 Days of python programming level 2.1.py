5f2c78612fd606e9ddc26688fbd69da632e02d3f
STROMtROPPAH <osurya254@gmail.com>
2026-07-26 22:20:01 +0800

    Add files via upload

---
+++ #Day 9: 30 Days of python programming level 2.1.py
@@ -0,0 +1,9 @@
+fruits = ['banana', 'orange', 'mango', 'lemon']
+
+please = input("please enter fruit: ")
+
+
+if please in fruits:
+    print (f"there is {please} in fruit")
+else:
+    print("there is no such fruit in fruit list")
