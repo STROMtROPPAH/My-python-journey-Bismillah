9214a6e70e3dbf1d5fd49e5cfaf668f53dba1b34
STROMtROPPAH <osurya254@gmail.com>
2026-08-01 23:02:58 +0800

    Add files via upload

---
+++ #Day 15: 30 Days of python programming .py
@@ -0,0 +1,12 @@
+# 1. SyntaxError - missing closing parenthesis
+print("Hello World"
+
+# 2. NameError - using a variable that doesn't exist
+print(undefined_variable)
+
+# 3. IndexError - accessing an index that doesn't exist
+my_list = [1, 2, 3]
+print(my_list[10])
+
+# 4. ModuleNotFoundError - importing a module that doesn't exist
+import nonexistent_module
