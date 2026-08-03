eaf53933534c259cff127d4bb35729b5ab9aabf0
STROMtROPPAH <osurya254@gmail.com>
2026-08-03 22:01:40 +0800

    Add files via upload

---
+++ #Day 17: 30 Days of python programming .py
@@ -0,0 +1,10 @@
+
+
+names = ['Finland', 'Sweden', 'Norway', 'Denmark', 'Iceland', 'Estonia', 'Russia']
+
+
+*nordic_countries, es, ru = names
+
+print("Nordic countries:", nordic_countries)
+print("Estonia:", es)
+print("Russia:", ru)
