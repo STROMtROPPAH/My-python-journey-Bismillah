e506baef7e95a656ddd674f74cae793932e6b1b8
STROMtROPPAH <osurya254@gmail.com>
2026-08-07 17:22:13 +0800

    Add files via upload

---
+++ #Day 20: 30 Days of python programming .py
@@ -0,0 +1,6 @@
+sentence = '''%I $am@% a %tea@cher%, &and& I lo%#ve %tea@ching%;. There $is nothing; &as& mo@re rewarding as educa@ting &and& @emp%o@wering peo@ple. ;I found tea@ching m%o@re interesting tha@n any other %jo@bs. %Do@es thi%s mo@tivate yo@u to be a tea@cher!?'''
+
+import re
+
+clean = re.sub(r'^a-zA-Z\s]', '', sentence)
+print(clean)
